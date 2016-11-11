2d71dbd9ea02e4195fa76ea9b15f4d19b9a5d51a
mahesh ogale <sonuogale@gmail.com>
2016-11-11 15:13:57 -0600

    new view

---
+++ js/gallery.js
@@ -0,0 +1,43 @@
+var $overlay = $("<div id='overlay'></div>");
+var $image = $("<img>");
+/*var $caption = $("<p></p>");*/
+
+$overlay.append($image);
+
+$("#overlay_container").append($overlay);
+
+
+$("#imagesContainer a").click(function( e ){
+	e.preventDefault();
+	var href = $(this).attr("href");
+	
+	$image.attr("src", href);
+	$("#imagesContainer img").css("opacity","0.3");
+	$("header").css("opacity","0.3");
+	$overlay.show();
+	
+	/*var caption_text = $(this).children("img").attr("alt");
+	$caption.text(caption_text);*/
+});
+
+$overlay.click(function(){
+	$("#imagesContainer img").css("opacity","1");
+	$("header").css("opacity","1");
+	$overlay.hide();
+});
+
+$( document ).on( 'click', function ( e ) {
+    if ( $( e.target ).closest( elem ).length === 0 ) {
+        $("#imagesContainer img").css("opacity","1");
+        $("header").css("opacity","1");
+        $overlay.hide();
+    }
+});
+
+$( document ).on( 'keydown', function ( e ) {
+    if ( e.keyCode === 27 ) { // ESC
+        $("#imagesContainer img").css("opacity","1");
+        $("header").css("opacity","1");
+        $overlay.hide();
+    }
+});
